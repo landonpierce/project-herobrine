6b2714820e479efebba2c3ca67da2d41e5ef740e
Ubuntu <azureuser@vidush-test-2.oueycijdvseexbu2ekjaaysq5a.bx.internal.cloudapp.net>
2022-09-22 19:15:42 +0000

    added azcopy auth and copy

--- agent/script.sh
+++ agent/script.sh
@@ -27,4 +27,7 @@ sudo cp ./azcopy_linux_amd64_10.16.0/azcopy /usr/bin/
 
 #Authorize Azcopy
 
+azcopy login --identity
+azcopy copy https://{storageAccountName}.blob.core.windows.net/{pathToWorld/world.tar.gz} ./  ##need to handle case where file isn't found i.e backup doesn't exist yet
+
 
